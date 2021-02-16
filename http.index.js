177b1dd1e992892ce03e7c1ba4417207e21c1f68
pyPlacca <dhegreatprosperity10@gmail.com>
2021-02-16 21:18:58 +0000

    add more routes

--- http.index.js
+++ http.index.js
@@ -3,22 +3,31 @@ const cfg = require('./config');
 
 
 function requestHandler (req, res) {
+	res.setHeader('content-type', 'text/html');
+
 	const respond = msg => {
-		res.write(msg)
-		res.end()
+		res.write(msg);
+		res.end();
 	}
 
 	switch (req.url) {
 		case '/':
-			return respond('Welcome')
+			return respond('Welcome');
+		case '/trends':
+			return respond('See what\'s trending');
+		case '/cart':
+			return respond('Items you add to cart will appear here.');
+		case '/about':
+			return respond('About us');
+		case '/contact':
+			return respond('Contact us');
 		default:
-			return respond('404. Page not found')
+			return respond('404. Page not found');
 	}
 }
 
-const server = http.createServer(requestHandler)
-
+const server = http.createServer(requestHandler);
 
 server.listen(cfg.port, cfg.host, () => {
-	console.log(`Server is running at ${cfg.host}:${cfg.port}`)
-})
+	console.log(`Http server is running at ${cfg.host}:${cfg.port}`)
+});
